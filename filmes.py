6cf366775e83fe66d16a5f0d529dd669d8b7c449
Gabriel Lima <34993129+HiroTatsuo@users.noreply.github.com>
2018-10-16 23:58:34 -0300

    Syntax adjustments

--- filmes.py
+++ filmes.py
@@ -1,5 +1,18 @@
+# -*- coding: utf-8 -*-
 class Filme:
+    """
+        Essa classe fornece um metodo construtor para novos objetos.
+        Para todo novo objeto criado um novo espaço em memoria é
+        reservado para ele e seus atributos, ou seja, copias dessas
+        variaveis são criadas.
+    """
     def __init__(self, titulo, endereco_poster, endereco_trailer):
+        """
+            titulo: String - Guarda do titulo do objeto
+            endereco_poster: String - Guarda uma URL do poster do objeto
+            endereco_trailer: String - Guarda uma URL do YouTube,
+                              é o trailer do objeto
+        """
         self.titulo = titulo
         self.endereco_poster = endereco_poster
         self.endereco_trailer = endereco_trailer
